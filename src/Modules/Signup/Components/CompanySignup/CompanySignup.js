0594a89ee1d4eb39f9ef949b477fe9a332c1b40d
Nancy Ahmed <anancy712@gmail.com>
2021-12-31 15:47:32 +0200

    register

--- src/Modules/Signup/Components/CompanySignup/CompanySignup.js
+++ src/Modules/Signup/Components/CompanySignup/CompanySignup.js
@@ -1,11 +1,27 @@
-import React from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
-import arrowRight from "../../../../Resources/Assets/arrow-right.svg";
+import React, { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import one from "../../../../Resources/Assets/Group 6499.png";
 import two from "../../../../Resources/Assets/Group 6498.png";
-
+import img from "../../../../Resources/Assets/Group 6026.png";
 import "./CompanySignup.css";
-function CompanySignup() {
+import { useSelector } from "react-redux";
+function CompanySignup() {const [email, setEmail] = useState("");
+  const [alert, setAlert] = useState(false);
+
+  const { currentLocal } = useSelector((state) => state.currentLocal);
+
+  const handleChange = (e) => {
+    const id = e.target.id;
+    switch (id) {
+      case "email": {
+        setEmail(e.target.value);
+        setAlert(false);
+        break;
+      }
+      default:
+        break;
+    }
+  };
   const sendData = (e) => {
     e.preventDefault();
     console.log("gi");
@@ -16,7 +32,7 @@ function CompanySignup() {
       <Container>
         <div className="CompanySignupContainer">
           <Row className="h-100">
-            <Col md={6} xs={12}>
+            <Col md={6} xs={12} className="CompanySignupCol">
               <div className="formContainer">
                 <h4>Sign up !</h4>
                 <p className="numImg">
@@ -28,75 +44,74 @@ function CompanySignup() {
                   </span>
                 </p>
                 <p>Company info</p>
-                <Form onSubmit={sendData}>
-                  <Container fluid className="p-0">
-                    <Row>
-                      <Col md={12}>
-                        <label>Company Name</label>
-                        <input
-                          type="text"
-                          className="w-100 input"
-                          id="Company Name"
-                        />
-                      </Col>
-                      <Col md={12}>
-                        <label>Selector</label>
-                        <input
-                          type="text"
-                          className="w-100 input"
-                          id="Selector"
-                        />
-                      </Col>
-                      <Col md={12}>
-                        <label>Email</label>
-                        <input
-                          type="email"
-                          className="w-100 input"
-                          id="email"
-                        />
-                      </Col>
-                      <Col md={12}>
+                <form onSubmit={sendData} className="forgetPassForm">
+                  <div className="form">
+                    <div>
+                      <div className="errorMsg">
+                        {alert && !email && (
+                          <div className="f-12">
+                            *{currentLocal.login.emailIsRequired}
+                          </div>
+                        )}
+                      </div>
+                      <Row>
+                        <Col md={12}>
+                          <label>Name</label>
+                          <input
+                            type="text"
+                            className="w-100  input"
+                            id="userName"
+                          />
+                        </Col>
+                        <Col md={12}>
+                          <label>Surname</label>
+
+                          <input
+                            type="text"
+                            className="w-100 input"
+                            id="Surname"
+                          />
+                        </Col>
+                        <Col md={12}>
+                          <label>Job</label>
+
+                          <input
+                            type="text"
+                            className="w-100 input"
+                            id="userName"
+                          />
+                        </Col>
+                        <Col md={12}>
+                          <label>Email</label>
+
+                          <input
+                            type="email"
+                            className="w-100 input"
+                            id="userName"
+                          />
+                        </Col>
+                        <Col md={12}>
                         <label>Phone Number</label>
-                        <input
-                          type="number"
-                          className="w-100 input"
-                          id="Phone Number"
-                        />
-                      </Col>
-                      <Col md={6}>
-                        <label>Country</label>
-                        <input
-                          type="text"
-                          className="w-100 input"
-                          id="country"
-                        />
-                      </Col>
-                      <Col md={6}>
-                        <label>City</label>
-                        <input type="text" className="w-100 input" id="city" />
-                      </Col>
-                      <Col md={12}>
-                        <label>Address</label>
-                        <input
-                          type="text"
-                          className="w-100 input mb-4"
-                          id="Address"
-                        />
-                      </Col>
-                    </Row>
-                  </Container>
-                  <Row>
-                    <div className="button">
-                      <button type="submit">
-                        Next
-                        <img src={arrowRight} alt="arrowRight" />
-                      </button>
+                          <input
+                            type="number"
+                            className="w-100 input"
+                            id="userName"
+                          />
+                        </Col>
+                      </Row>
                     </div>
-                  </Row>
-                </Form>
+                  </div>
+                  <div className="button">
+                    <button type="submit" className="button-primary">
+                      {currentLocal.login.send}
+                    </button>
+                  </div>
+                </form>
               </div>
             </Col>
-            <Col md={6} xs={12} className="img-container"></Col>
+            <Col md={6} xs={12} className="img-container">
+              <img src={img} alt="img" className="w-100"/>
+            </Col>
           </Row>
         </div>
       </Container>{" "}
--- src/Modules/Signup/Components/userSignup/UserSignup.js
+++ src/Modules/Signup/Components/userSignup/UserSignup.js
@@ -1,10 +1,28 @@
-import React from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import img from "../../../../Resources/Assets/Group 6026.png";
 import "./UserSignup.css";
 function UserSignup() {
+  const [email, setEmail] = useState("");
+  const [alert, setAlert] = useState(false);
+
+  const { currentLocal } = useSelector((state) => state.currentLocal);
   const sendData = (e) => {
     e.preventDefault();
-    console.log("gi");
+    console.log("hi");
+  };
+  const handleChange = (e) => {
+    const id = e.target.id;
+    switch (id) {
+      case "email": {
+        setEmail(e.target.value);
+        setAlert(false);
+        break;
+      }
+      default:
+        break;
+    }
   };
   return (
     <div className="UserSignup">
@@ -18,69 +36,81 @@ function UserSignup() {
                   <span
                     className="mx-5"
                     onClick={() => {
-                      localStorage.removeItem(
-                        "signupState"
-                      );
+                      localStorage.removeItem("signupState");
                     }}
                   >
                     First
                   </span>
                   <span>last</span>
                 </p>
-                <Form onSubmit={sendData}>
-                  <Container fluid className="p-0">
-                    <Row>
-                      <Col md={12}>
-                        <input
-                          type="text"
-                          placeholder="Name"
-                          className="w-100 mb-4"
-                          id="userName"
-                        />
-                      </Col>
-                      <Col md={12}>
-                        <input
-                          type="text"
-                          placeholder="Surname"
-                          className="w-100 mb-4"
-                          id="userName"
-                        />
-                      </Col>
-                      <Col md={12}>
-                        <input
-                          type="text"
-                          placeholder="Job"
-                          className="w-100 mb-4"
-                          id="userName"
-                        />
-                      </Col>
-                      <Col md={12}>
-                        <input
-                          type="email"
-                          placeholder="Email"
-                          className="w-100 mb-4"
-                          id="userName"
-                        />
-                      </Col>
-                      <Col md={12}>
-                        <input
-                          type="number"
-                          placeholder="Phone Number"
-                          className="w-100 mb-4"
-                          id="userName"
-                        />
-                      </Col>
-                    </Row>
-                  </Container>
-                  <Row>
-                    <div className="button">
-                      <button type="submit">Finish </button>
+                <form onSubmit={sendData} className="forgetPassForm">
+                  <div className="form">
+                    <div>
+                      <div className="errorMsg">
+                        {alert && !email && (
+                          <div className="f-12">
+                            *{currentLocal.login.emailIsRequired}
+                          </div>
+                        )}
+                      </div>
+                      <Row>
+                        <Col md={12}>
+                          <label>Name</label>
+                          <input
+                            type="text"
+                            className="w-100  input"
+                            id="userName"
+                          />
+                        </Col>
+                        <Col md={12}>
+                          <label>Surname</label>
+
+                          <input
+                            type="text"
+                            className="w-100 input"
+                            id="Surname"
+                          />
+                        </Col>
+                        <Col md={12}>
+                          <label>Job</label>
+
+                          <input
+                            type="text"
+                            className="w-100 input"
+                            id="userName"
+                          />
+                        </Col>
+                        <Col md={12}>
+                          <label>Email</label>
+
+                          <input
+                            type="email"
+                            className="w-100 input"
+                            id="userName"
+                          />
+                        </Col>
+                        <Col md={12}>
+                        <label>Phone Number</label>
+                          <input
+                            type="number"
+                            className="w-100 input"
+                            id="userName"
+                          />
+                        </Col>
+                      </Row>
                     </div>
-                  </Row>
-                </Form>
+                  </div>
+                  <div className="button">
+                    <button type="submit" className="button-primary">
+                      {currentLocal.login.send}
+                    </button>
+                  </div>
+                </form>
               </div>
             </Col>
-            <Col md={6} xs={12} className="img-container"></Col>
+            <Col md={6} xs={12} className="img-container">
+              <img src={img} alt="img" />
+            </Col>
           </Row>
         </div>
       </Container>{" "}
